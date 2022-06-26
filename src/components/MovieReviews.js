1fafff616f921c115bddcd55958e27d7d4b73d6a
danielshires <hello@danielshires.com>
2022-06-26 10:43:13 +1000

    Initial Commit

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -2,9 +2,7 @@
 import React from "react"
 
 function renderMovieReviews(props) {
-    console.log(props.movieFetchResponse)
     return (
-    
         <li className="review-list">
             <div>{props.movieFetchResponse.display_title}</div>
             <div>{props.movieFetchResponse.summary_short}</div>
